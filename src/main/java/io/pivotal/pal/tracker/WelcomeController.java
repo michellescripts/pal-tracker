52a72c97c35ca6694ca469cbb7eb2ebca041e526
michellescripts <m@michellescripts.com>
2018-11-01 11:42:00 -0600

    Complete Deploy a Spring Boot App
    - Assignment: cloudNativeDeveloperCloudFoundry
    - Deployed to: http://mb-pal-tracker-excellent-jackal.cfapps.io

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String hello;
+
+    public WelcomeController(
+        @Value("${WELCOME_MESSAGE}") String message
+    ) {
+        this.hello = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
     }
 }
